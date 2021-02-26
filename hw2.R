9b08d4eb41c1cdf2f90851516826c64b657632cb
yu-syuan-tsai <79685838+yu-syuan-tsai@users.noreply.github.com>
2021-02-26 15:47:21 +0800

    Add files via upload

---
+++ hw2.R
@@ -0,0 +1,27 @@
+---
+  title: "HW2"
+author: "蔡宇媗(H24086016)"
+output: 
+  ---
+
+# Problem 1: Use the function summary() to obtain summary statistics for the Iris dataset 
+
+iris=c(1,2,3,4,5)
+summary(iris)
+
+
+# Problem 2:
+
+#The sample median of the variable Petail.With is 
+
+Petail.With=c(1,2,3,4,5)
+mean(Petail.With)
+
+# Problem 3: Draw a histogram for the variable "Sepal.Length"
+
+Sepal.Length=c(1,2,3,4,5)
+hist(Sepal.Length)
+
+# Problem 4: Draw a scatter plot
+A=c(1,2,3,4,5)
+plot(A)
